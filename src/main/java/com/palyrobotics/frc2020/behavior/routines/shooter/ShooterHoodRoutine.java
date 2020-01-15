c2fdbf360431890757046c2d514201b7abfad133
Nolan You <nolanyou35@gmail.com>
2020-01-14 17:53:41 -0800

    added in solenoid routines for shooterHoodRoutine , still need to implement start and finish at some point.

--- src/main/java/com/palyrobotics/frc2020/behavior/routines/shooter/ShooterHoodRoutine.java
+++ src/main/java/com/palyrobotics/frc2020/behavior/routines/shooter/ShooterHoodRoutine.java
@@ -1,5 +1,6 @@
 package com.palyrobotics.frc2020.behavior.routines.shooter;
 
+import com.palyrobotics.frc2020.behavior.ParallelRoutine;
 import com.palyrobotics.frc2020.behavior.Routine;
 import com.palyrobotics.frc2020.behavior.SequentialRoutine;
 import com.palyrobotics.frc2020.behavior.routines.TimeoutRoutine;
@@ -8,10 +9,15 @@ import com.palyrobotics.frc2020.subsystems.Shooter;
 import com.palyrobotics.frc2020.subsystems.Subsystem;
 
 public class ShooterHoodRoutine extends Routine {
-    //todo: implement, add counter thing to time
+    //todo: add start and finish
+    Shooter.HoodState mHoodState;
     public ShooterHoodRoutine(Shooter.HoodState mHoodState) {
         super();
+        this.mHoodState = mHoodState;
     }
+
+    Routine horizontal;
+    Routine vertical;
 
     @Override
     public void start() {
@@ -20,12 +26,36 @@ public class ShooterHoodRoutine extends Routine {
 
     @Override
     public Commands update(Commands commands) {
-        return null;
+        switch(mHoodState){
+            case LOW:
+                vertical = new VerticalShooterHoodRoutine(false);
+                horizontal = new HorizontalShooterHoodRoutine(false);
+                commands.addWantedRoutine(new SequentialRoutine(vertical, horizontal));
+                break;
+            case MEDIUM:
+                //moves vertical one up first, waits, sends out the horizontal one, waits, pulls down the vertical one
+                vertical = new VerticalShooterHoodRoutine(true);
+                horizontal = new ParallelRoutine(new HorizontalShooterHoodRoutine(true), new TimeoutRoutine(0.3));
+                Routine vertical2 = new ParallelRoutine(new TimeoutRoutine(0.3), new VerticalShooterHoodRoutine(false));
+                commands.addWantedRoutine(new SequentialRoutine(vertical, horizontal, vertical2));
+                break;
+            case HIGH:
+                vertical = new VerticalShooterHoodRoutine(true);
+                horizontal = new HorizontalShooterHoodRoutine(false);
+                commands.addWantedRoutine(new SequentialRoutine(vertical, horizontal));
+                break;
+
+        }
+        return commands;
     }
 
     @Override
     public Commands cancel(Commands commands) {
-        return null;
+        //retracts the horizontal one first, waits, then retracts the vertical one.
+        horizontal = new HorizontalShooterHoodRoutine(true);
+        Routine vertical2 = new ParallelRoutine(new TimeoutRoutine(0.3), new VerticalShooterHoodRoutine(false));
+        commands.addWantedRoutine(new SequentialRoutine(horizontal, vertical2));
+        return commands;
     }
 
     @Override
