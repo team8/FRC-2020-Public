8fea8c0998eeca500a63f4daf1eb14a3519ccea0
Quintin Dwight <qhdwight@gmail.com>
2020-02-02 11:50:04 -0800

    Documentation for timed solenoid

--- src/main/java/com/palyrobotics/frc2020/util/control/TimedSolenoid.java
+++ src/main/java/com/palyrobotics/frc2020/util/control/TimedSolenoid.java
@@ -5,6 +5,11 @@ import com.palyrobotics.frc2020.util.Util;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
 
+/**
+ * Solenoid connected to piston(s) that uses a {@link Timer} to estimate when the piston(s)
+ * physically changes extension state. It does not account for environmental errors that cause the
+ * piston(s) to take more time extending or retracting than initially empirically measured.
+ */
 public class TimedSolenoid extends Solenoid {
 
 	private final double mExtensionDurationSeconds;
@@ -12,14 +17,27 @@ public class TimedSolenoid extends Solenoid {
 	private Timer mTimer = new Timer();
 	private boolean mIsExtended;
 
+	/**
+	 * @param channel                  PCM channel 0-7.
+	 * @param extensionDurationSeconds Estimated time in seconds it takes the piston to retract or
+	 *                                 extend.
+	 * @param isExtendedByDefault      Whether or not the default solenoid state causes the piston to
+	 *                                 extend.
+	 */
 	public TimedSolenoid(int channel, double extensionDurationSeconds, boolean isExtendedByDefault) {
 		super(channel);
 		mExtensionDurationSeconds = extensionDurationSeconds;
 		mIsExtendedByDefault = isExtendedByDefault;
 	}
 
+	/**
+	 * Updates piston(s) extension state based on how long our solenoid has been in a specific on/off
+	 * state.
+	 *
+	 * @param isOn Solenoid on/off state
+	 */
 	public void updateExtended(boolean isOn) {
-		// Account for default state of piston given solenoid state
+		// Account for default state of piston(s) given solenoid state
 		boolean shouldBeExtended = mIsExtendedByDefault == isOn;
 		if (mIsExtended != shouldBeExtended && Util.approximatelyEqual(mTimer.get(), 0)) {
 			mTimer.start();
@@ -31,6 +49,9 @@ public class TimedSolenoid extends Solenoid {
 		}
 	}
 
+	/**
+	 * @return Whether or not the piston(s) controlled by this solenoid are extended.
+	 */
 	public boolean isExtended() {
 		return mIsExtended;
 	}
