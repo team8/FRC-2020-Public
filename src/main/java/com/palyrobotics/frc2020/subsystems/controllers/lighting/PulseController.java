b24fb0fa6b6c2baff352328b41181ede3f7c0251
Aditya-Paly <aoberai05@gmail.com>
2020-02-10 00:58:31 -0800

    code clean up, switched weird delay sequences to wpilib timer (needs to be tested), added timer into parent class instead of reinstantiating in each controller

--- src/main/java/com/palyrobotics/frc2020/subsystems/controllers/lighting/PulseController.java
+++ src/main/java/com/palyrobotics/frc2020/subsystems/controllers/lighting/PulseController.java
@@ -5,8 +5,6 @@ import com.palyrobotics.frc2020.robot.RobotState;
 import com.palyrobotics.frc2020.subsystems.Lighting;
 import com.palyrobotics.frc2020.util.Color;
 
-import edu.wpi.first.wpilibj.Timer;
-
 public class PulseController extends Lighting.LEDController {
 
 	private Color.HSV[] mPulse;
@@ -27,7 +25,7 @@ public class PulseController extends Lighting.LEDController {
 
 	@Override
 	public void updateSignal(Commands commands, RobotState state) {
-		if (mTimer.hasPeriodPassed(1/mSpeed)) {
+		if (mTimer.hasPeriodPassed(1 / mSpeed)) {
 			mInitIndex += 1;
 			mLastIndex += 1;
 		}
