58b9b285aa6d13acaeb44598b90377c1cbcaea68
Aditya-Paly <aoberai05@gmail.com>
2020-02-21 11:24:38 -0800

    CircularInterpolatingTreeMap extends Interpolating tree map now for cleanup

--- src/main/java/com/palyrobotics/frc2020/util/CircularInterpolatingDoubleTreeMap.java
+++ src/main/java/com/palyrobotics/frc2020/util/CircularInterpolatingDoubleTreeMap.java
@@ -1,14 +1,12 @@
 package com.palyrobotics.frc2020.util;
 
-import java.util.TreeMap;
-
 import edu.wpi.first.wpilibj.Timer;
 
 /**
  * Interpolating tree maps are used to get values at points that are not defined by making a guess
  * from points that are defined using linear interpolation.
  */
-public class CircularInterpolatingDoubleTreeMap extends TreeMap<Double, Double> {
+public class CircularInterpolatingDoubleTreeMap extends InterpolatingDoubleTreeMap {
 
 	private final int mWindowSize;
 
@@ -16,51 +14,6 @@ public class CircularInterpolatingDoubleTreeMap extends TreeMap<Double, Double> 
 		mWindowSize = windowSize;
 		while (size() < mWindowSize) {
 			put(Timer.getFPGATimestamp(), 0.0);
-		}
-	}
-
-	public double interpolate(double low, double high, double normalizedValue) {
-		if (high < low) {
-			throw new IllegalArgumentException("Upper bound was not greater or equal to lower bound!");
-		}
-		double delta = high - low;
-		return delta * normalizedValue + low;
-	}
-
-	public double inverseInterpolate(double low, double high, double interpolatedValue) {
-		double upperToLower = high - low;
-		if (upperToLower <= 0.0) {
-			return 0.0;
-		}
-		double interpolatedToLower = interpolatedValue - low;
-		if (interpolatedToLower <= 0.0) {
-			return 0.0;
-		}
-		return interpolatedToLower / upperToLower;
-	}
-
-	/**
-	 * @param  key Lookup for a key (does not have to exist)
-	 * @return     Interpolated value or null
-	 */
-	public Double getInterpolated(double key) {
-		Double retrievedValue = get(key);
-		if (retrievedValue == null) {
-			// Get surrounding keys for interpolation
-			Double upperKeyBound = ceilingKey(key), lowerKeyBound = floorKey(key);
-			// If attempting interpolation at ends of tree, return the nearest data point
-			if (upperKeyBound == null && lowerKeyBound == null) {
-				return null;
-			} else if (upperKeyBound == null) {
-				return get(lowerKeyBound);
-			} else if (lowerKeyBound == null) {
-				return get(upperKeyBound);
-			}
-			// get surrounding values for interpolation
-			Double upperValue = get(upperKeyBound), lowerValue = get(lowerKeyBound);
-			return interpolate(lowerValue, upperValue, inverseInterpolate(lowerKeyBound, upperKeyBound, key));
-		} else {
-			return retrievedValue;
 		}
 	}
 
