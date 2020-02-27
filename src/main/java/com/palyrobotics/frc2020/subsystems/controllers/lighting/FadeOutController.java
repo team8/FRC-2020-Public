d39b7280fffe5b4c99d007d27bf6ef6accc76d13
Alexis Kim <ajkim0630@gmail.com>
2020-02-26 16:01:24 -0800

    fade controllers

---
+++ src/main/java/com/palyrobotics/frc2020/subsystems/controllers/lighting/FadeOutController.java
@@ -0,0 +1,58 @@
+package com.palyrobotics.frc2020.subsystems.controllers.lighting;
+
+import com.palyrobotics.frc2020.robot.Commands;
+import com.palyrobotics.frc2020.robot.RobotState;
+import com.palyrobotics.frc2020.subsystems.Lighting;
+import com.palyrobotics.frc2020.util.Color;
+
+public class FadeOutController extends Lighting.LEDController {
+
+	private Color.HSV mFlashedColor;
+	private int mDuration = -1;
+
+	/**
+	 * Color flashes with given delay
+	 *
+	 * @param startIndex   Initial index upon which led patterns should start
+	 * @param lastIndex    End index upon which led patterns should stop
+	 * @param flashedColor Color to be flashed on white background
+	 */
+
+	public FadeOutController(int startIndex, int lastIndex, Color.HSV flashedColor, int delay) {
+		super(startIndex, lastIndex);
+		mStartIndex = startIndex;
+		mLastIndex = lastIndex;
+		mFlashedColor = flashedColor;
+		mSpeed = delay == 0 ? kZeroSpeed : delay;
+		mTimer.start();
+	}
+
+	public FadeOutController(int startIndex, int lastIndex, Color.HSV flashedColor, int delay, int duration) {
+		super(startIndex, lastIndex);
+		mStartIndex = startIndex;
+		mLastIndex = lastIndex;
+		mFlashedColor = flashedColor;
+		mSpeed = delay == 0 ? kZeroSpeed : delay;
+		mDuration = duration;
+		mTimer.start();
+	}
+
+	@Override
+	public void updateSignal(Commands commands, RobotState state) {
+
+		double n = 1 - ((mTimer.get() % mSpeed) / mSpeed);
+
+		for (int i = mStartIndex; i < mLastIndex; i++) {
+
+			mOutputs.lightingOutput.get(i - mStartIndex).setHSV(mFlashedColor.getH(),
+					mFlashedColor.getS(),
+					(int) (mFlashedColor.getV() * n));
+		}
+
+	}
+
+	@Override
+	public boolean checkFinished() {
+		return mDuration != -1 && mTimer.get() > mDuration;
+	}
+}
