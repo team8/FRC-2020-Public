332e914c79863e8527de419bbc0de3b49963c2cd
JasonLiu2002 <realjasonliu@gmail.com>
2020-02-28 17:16:01 -0800

    pbot stuff

--- src/main/java/com/palyrobotics/frc2020/robot/HardwareWriter.java
+++ src/main/java/com/palyrobotics/frc2020/robot/HardwareWriter.java
@@ -133,8 +133,8 @@ public class HardwareWriter {
 			vTalon.configSupplyCurrentLimit(k30AmpCurrentLimitConfiguration, kTimeoutMs);
 			vTalon.configFrameTimings(40, 40);
 		}
-		hardware.leftVTalon.setInverted(true);
-		hardware.rightVTalon.setInverted(true);
+		hardware.leftVTalon.setInverted(true); // Cbot false
+		hardware.rightVTalon.setInverted(false); // Cbot true
 	}
 
 	private void configureIntakeHardware() {
@@ -160,7 +160,7 @@ public class HardwareWriter {
 		hardware.masterSpark.restoreFactoryDefaults();
 		hardware.slaveSpark.restoreFactoryDefaults();
 		hardware.slaveSpark.follow(hardware.masterSpark, true);
-		hardware.masterSpark.setInverted(true);
+		hardware.masterSpark.setInverted(false);
 		/* Flywheel velocity in RPM, adjusted for gearing ratio */
 		hardware.masterEncoder.setVelocityConversionFactor(1.0 / 0.76923076);
 		// TODO: Current limiting and closed/open loop ramp rates
