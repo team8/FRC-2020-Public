ea0d1fabd7b71c16f1005c79ef43cdf2c3420562
Zach Szeto <zszeto@Zachs-MacBook-Air.local>
2021-10-07 19:03:59 -0700

    added auto and points

---
+++ src/main/java/com/palyrobotics/frc2020/auto/StartCenterShootThreeRendezvousFiveShootFive.java
@@ -0,0 +1,43 @@
+package com.palyrobotics.frc2020.auto;
+
+import com.palyrobotics.frc2020.behavior.ParallelRoutine;
+import com.palyrobotics.frc2020.behavior.RoutineBase;
+import com.palyrobotics.frc2020.behavior.SequentialRoutine;
+import com.palyrobotics.frc2020.behavior.routines.TimedRoutine;
+import com.palyrobotics.frc2020.behavior.routines.drive.DriveParallelPathRoutine;
+import com.palyrobotics.frc2020.behavior.routines.drive.DrivePathRoutine;
+import com.palyrobotics.frc2020.behavior.routines.drive.DriveSetOdometryRoutine;
+import com.palyrobotics.frc2020.behavior.routines.superstructure.IndexerFeedAllRoutine;
+import com.palyrobotics.frc2020.behavior.routines.superstructure.IntakeBallRoutine;
+import com.palyrobotics.frc2020.behavior.routines.superstructure.ShooterVisionRoutine;
+
+import static com.palyrobotics.frc2020.util.Util.newWaypoint;
+
+public class StartCenterShootThreeRendezvousFiveShootFive extends AutoBase {
+
+    @Override
+    public RoutineBase getRoutine() {
+        var setInitialOdometry = new DriveSetOdometryRoutine(126, 200, 180);
+        var initialShoot = new ParallelRoutine(
+                new ShooterVisionRoutine(3.0),
+                new SequentialRoutine(
+                        new TimedRoutine(0.8),
+                        new IndexerFeedAllRoutine(2.2, false, true)));
+        var getBalls = new SequentialRoutine(
+                new DrivePathRoutine(newWaypoint(211, 171, 200))
+                        .setMovement(1.6 , 1.4),
+                new IntakeBallRoutine(1),
+                new DrivePathRoutine(newWaypoint(294, 210, 160))
+                        .setMovement(1.6, 1.4),
+                new DrivePathRoutine(newWaypoint(298, 182, 250))
+                        .setMovement(1.6, 1.4),
+                new DrivePathRoutine(newWaypoint(216, 147, 270))
+                        .setMovement(1.6, 1.4));
+        var shoot = new SequentialRoutine(
+                new ShooterVisionRoutine(5),
+                new SequentialRoutine(
+                        new TimedRoutine(0.2),
+                        new IndexerFeedAllRoutine(3.8, false, true)));
+        return new SequentialRoutine(setInitialOdometry, initialShoot, getBalls, shoot);
+    }
+}
