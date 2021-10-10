8fd0e455244b2451b72da296fa10d53c90a72fad
Zach Szeto <zszeto@Zachs-MacBook-Air.local>
2021-10-10 15:09:40 -0700

    changed starting pos for auto

--- src/main/java/com/palyrobotics/frc2020/auto/StartCenterShootThreeRendezvousFiveShootFive.java
+++ src/main/java/com/palyrobotics/frc2020/auto/StartCenterShootThreeRendezvousFiveShootFive.java
@@ -16,27 +16,31 @@ public class StartCenterShootThreeRendezvousFiveShootFive extends AutoBase {
 
     @Override
     public RoutineBase getRoutine() {
-        var setInitialOdometry = new DriveSetOdometryRoutine(126, 200, 180);
+        var setInitialOdometry = new DriveSetOdometryRoutine(126, 130, 130); //Should be ~50 degrees
+
         var initialShoot = new ParallelRoutine(
                 new ShooterVisionRoutine(3.0),
                 new SequentialRoutine(
                         new TimedRoutine(0.8),
                         new IndexerFeedAllRoutine(2.2, false, true)));
-        var getBalls = new SequentialRoutine(
+
+        var getBalls = new ParallelRoutine(new SequentialRoutine(
                 new DrivePathRoutine(newWaypoint(211, 171, 19))
                         .setMovement(1.6 , 1.4),
-                new IntakeBallRoutine(1),
                 new DrivePathRoutine(newWaypoint(294, 210, -25))
                         .setMovement(1.6, 1.4),
                 new DrivePathRoutine(newWaypoint(298, 182, 85))
                         .setMovement(1.6, 1.4),
                 new DrivePathRoutine(newWaypoint(216, 147, 157))
-                        .setMovement(1.6, 1.4));
+                        .setMovement(1.6, 1.4)),
+        new IntakeBallRoutine(5)); //tune value
+
         var shoot = new SequentialRoutine(
                 new ShooterVisionRoutine(5),
                 new SequentialRoutine(
                         new TimedRoutine(0.2),
                         new IndexerFeedAllRoutine(3.8, false, true)));
+        
         return new SequentialRoutine(setInitialOdometry, initialShoot, getBalls, shoot);
     }
 }
