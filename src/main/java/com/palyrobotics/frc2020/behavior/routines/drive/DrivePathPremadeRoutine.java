3c0b23d259f6a8e5d182b8070f1c2084140b2e9b
Nolan You <nolanyou35@gmail.com>
2021-10-12 17:03:10 -0700

    added DrivePathPremadeRoutine

--- src/main/java/com/palyrobotics/frc2020/behavior/routines/drive/DrivePathPremadeRoutine.java
+++ src/main/java/com/palyrobotics/frc2020/behavior/routines/drive/DrivePathPremadeRoutine.java
@@ -1,19 +1,16 @@
 package com.palyrobotics.frc2020.behavior.routines.drive;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.palyrobotics.frc2020.util.TrajectoryReader.TrajectoryReader;
+
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+public class DrivePathPremadeRoutine extends DrivePathRoutine {
 
-public class DrivePathPremadeRoutine extends DrivePathRoutine{
-    public DrivePathPremadeRoutine(String trajectoryFile) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        Trajectory obj = mapper.readValue(new File(trajectoryFile), Trajectory.class);
-        var states = new ArrayList<Trajectory.State>();
-
-
-        //json open trajfile, read it
-    }
+	public List<Trajectory.State> generateTrajectory(String trajectoryFile) throws JsonProcessingException {
+		TrajectoryReader trajReader = new TrajectoryReader(trajectoryFile);
+		return trajReader.getTrajectory();
+	}
 }
---
+++ src/main/java/com/palyrobotics/frc2020/util/TrajectoryReader/TrajectoryReader.java
@@ -0,0 +1,26 @@
+package com.palyrobotics.frc2020.util.TrajectoryReader;
+
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+
+public class TrajectoryReader {
+
+	private String jsonString;
+
+	public TrajectoryReader(String jsonString) {
+		this.jsonString = jsonString;
+	}
+
+	public List<Trajectory.State> getTrajectory() throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		List<Trajectory.State> out = objectMapper.readValue(jsonString, new TypeReference<List<Trajectory.State>>() {
+		});
+		System.out.println(out);
+		return out;
+	}
+}
