25d111b75de1b83384dc4d33b5eb4d75ea1f4e2d
Zach Szeto <zszeto@Zachs-MacBook-Air.local>
2021-10-16 12:03:34 -0700

    changed coordinates and angles

--- src/main/java/com/palyrobotics/frc2020/auto/StartCenterShootThreeRendezvousFiveShootFive.java
+++ src/main/java/com/palyrobotics/frc2020/auto/StartCenterShootThreeRendezvousFiveShootFive.java
@@ -28,17 +28,20 @@ public class StartCenterShootThreeRendezvousFiveShootFive extends AutoBase {
 								new IndexerFeedAllRoutine(2.2, false, true))));
 
 		var getBalls = new ParallelRoutine(new SequentialRoutine(
-				new DrivePathRoutine(newWaypoint(211, 171, 19))
+				new DrivePathRoutine(newWaypoint(211, 171, 334))
 						.setMovement(1.6, 1.4),
-				new DrivePathRoutine(newWaypoint(294, 210, -25))
+				new DrivePathRoutine(newWaypoint(298, 209, 38))
 						.setMovement(1.6, 1.4),
-				new DrivePathRoutine(newWaypoint(298, 182, 85))
+				new DrivePathRoutine(newWaypoint(311, 199, 126))
 						.setMovement(1.6, 1.4),
-				new DrivePathRoutine(newWaypoint(216, 147, 157))
+				new DrivePathRoutine(newWaypoint(295, 177, 159))
+						.setMovement(1.6, 1.4),
+				new DrivePathRoutine(newWaypoint(217, 147, 159))
 						.setMovement(1.6, 1.4)),
-				new IntakeBallRoutine(5));
+				new IntakeBallRoutine(8));
 
 		var shoot = new SequentialRoutine(
+				new DriveYawRoutine(200),
 				new ShooterVisionRoutine(5),
 				new SequentialRoutine(
 						new TimedRoutine(0.2),
