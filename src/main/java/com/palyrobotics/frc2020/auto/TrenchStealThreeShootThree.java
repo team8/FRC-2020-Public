f39e292061f16887038f5fd7051255378b3a8544
Pranav Adya <pranavadya@gmail.com>
2021-10-26 17:57:27 -0700

    created shootingPos var. Trying not to look at another auto, if someone could tell me if im on right track let me know..

--- src/main/java/com/palyrobotics/frc2020/auto/TrenchStealThreeShootThree.java
+++ src/main/java/com/palyrobotics/frc2020/auto/TrenchStealThreeShootThree.java
@@ -1,5 +1,6 @@
 package com.palyrobotics.frc2020.auto;
 
+import com.palyrobotics.frc2020.behavior.ParallelRoutine;
 import com.palyrobotics.frc2020.behavior.RoutineBase;
 import com.palyrobotics.frc2020.behavior.SequentialRoutine;
 import com.palyrobotics.frc2020.behavior.routines.ParallelRaceRoutine;
@@ -17,12 +18,15 @@ public class TrenchStealThreeShootThree extends AutoBase {
     public RoutineBase getRoutine() {
         var setInitialOdometry = new DriveSetOdometryRoutine(0, 0, 0);
 
-        var getTrenchBalls = new ParallelRaceRoutine(
+        var intakeTrenchBalls = new ParallelRaceRoutine(
                 new SequentialRoutine(
                         new DrivePathRoutine(newWaypoint(88, 0, 0)).setMovement(1.5, 2.5),
                         new DriveYawRoutine(12.0)),
                 new IndexerTimeRoutine(Double.POSITIVE_INFINITY),
-                new IntakeBallRoutine(Double.POSITIVE_INFINITY, 1.0));
-        return new SequentialRoutine(setInitialOdometry, getTrenchBalls);
+                new IntakeBallRoutine(Double.POSITIVE_INFINITY, 2.0));
+
+        var shootingPos = new ParallelRoutine(new DrivePathRoutine(newWaypoint(50, 150, 180)).setMovement(3.0, 5.0).driveInReverse());
+
+        return new SequentialRoutine(setInitialOdometry, intakeTrenchBalls);
     }
 }
