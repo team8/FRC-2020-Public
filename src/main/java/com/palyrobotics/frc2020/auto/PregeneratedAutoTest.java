b6bab73158f1131da6d6f369a8fb0ebbad0e6ba5
Nolan You <nolanyou35@gmail.com>
2021-11-21 14:28:09 -0800

    added some spicy predicated wait routines into a pathweaver test auto, needs testing

--- src/main/java/com/palyrobotics/frc2020/auto/PregeneratedAutoTest.java
+++ src/main/java/com/palyrobotics/frc2020/auto/PregeneratedAutoTest.java
@@ -1,15 +1,35 @@
 package com.palyrobotics.frc2020.auto;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.palyrobotics.frc2020.behavior.ParallelRoutine;
 import com.palyrobotics.frc2020.behavior.RoutineBase;
+import com.palyrobotics.frc2020.behavior.SequentialRoutine;
 import com.palyrobotics.frc2020.behavior.routines.drive.DrivePathPremadeRoutine;
+import com.palyrobotics.frc2020.behavior.routines.drive.PredicateDriveWaitRoutine;
+import com.palyrobotics.frc2020.behavior.routines.superstructure.IntakeLowerRoutine;
+import com.palyrobotics.frc2020.util.Util;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.util.Units;
+
+import java.util.function.Predicate;
+
+import static com.palyrobotics.frc2020.util.Util.newWaypoint;
 
 public class PregeneratedAutoTest extends AutoBase {
 
 	@Override
 	public RoutineBase getRoutine() throws JsonProcessingException {
-		var temp = new DrivePathPremadeRoutine("2BallAuto.wpilib");
+		Predicate<Pose2d> inTrenchTest = poseMeters -> poseMeters.getTranslation().getX() > Units.inchesToMeters(30.0);
+		SequentialRoutine driveCollect = new SequentialRoutine(
+				new ParallelRoutine(
+						new DrivePathPremadeRoutine("DriveEnemyTrenchPt1.wpilib.json.wpilib"),
+						new SequentialRoutine(
+								new PredicateDriveWaitRoutine(inTrenchTest), new IntakeLowerRoutine()
+						)
+				),
+				new DrivePathPremadeRoutine("DriveEnemyTrenchPt2.wpilib.json.wpilib"));
 
-		return temp;
+	    return driveCollect;
 	}
 }
