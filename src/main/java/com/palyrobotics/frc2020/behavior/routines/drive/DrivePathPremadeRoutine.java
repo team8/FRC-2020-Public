ae18fd5474a6bbe5a244908f3f3ca231106130e4
Nolan You <nolanyou35@gmail.com>
2021-11-21 14:54:27 -0800

    updated drivePathPremadeRoutine to throw errors better + correct config file path

--- src/main/java/com/palyrobotics/frc2020/behavior/routines/drive/DrivePathPremadeRoutine.java
+++ src/main/java/com/palyrobotics/frc2020/behavior/routines/drive/DrivePathPremadeRoutine.java
@@ -12,17 +12,18 @@ import com.palyrobotics.frc2020.robot.RobotState;
 import com.palyrobotics.frc2020.util.Util;
 import com.palyrobotics.frc2020.util.config.Configs;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 
 public class DrivePathPremadeRoutine extends DrivePathRoutine {
 
-	private static final String kTrajFolderName = "built-trajectories", kLoggerTag = Util.classToJsonName(Configs.class);
+	private static final String kTrajFolderName = "output";
 	private static final Path kConfigFolder = Paths.get(Filesystem.getDeployDirectory().toString(), kTrajFolderName);
 
 	private static Path resolveConfigPath(String name) {
-		return kConfigFolder.resolve(String.format("%s.json", name));
+			return kConfigFolder.resolve(String.format("%s.json", name));
 	}
 
 	private String mTrajectoryFileName;
@@ -43,8 +44,9 @@ public class DrivePathPremadeRoutine extends DrivePathRoutine {
 		var resolvedConfigPath = resolveConfigPath(this.mTrajectoryFileName);
 		try {
 			super.mTrajectory = TrajectoryUtil.fromPathweaverJson(resolvedConfigPath);
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+	 	catch(IOException ex){
+				DriverStation.reportError("Unable to open trajectory", ex.getStackTrace());
+	 	}
 	}
 }
